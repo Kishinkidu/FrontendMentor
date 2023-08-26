5bad87dbff9d296cc91561e5e89b60249352b9f4
Kishinkidu <erick.revoredo58@gmail.com>
2023-08-26 16:15:50 -0500

    js: add congratulationbox, formcontainer,congrtulationmessage  html: div. congratulations

--- interactive-rating-component-main/scripts.js
+++ interactive-rating-component-main/scripts.js
@@ -9,21 +9,35 @@ function formulario() {
             this.classList.add("selected");
         });
     });
-
+    
     const submitButton = document.querySelector(".button");
     submitButton.addEventListener("click", function(event) {
         event.preventDefault();
-        
+        console.log(submitButton)
         const selectedRatingButton = document.querySelector(".number.selected");
         if (selectedRatingButton) {
             const selectedRating = selectedRatingButton.value;
             console.log(`Se envió la calificación ${selectedRating} al servidor`);
+            console.log(selectedRating)
+            const congratulationBox = document.getElementById("congratulations");
+            congratulationBox.style.display = "flex";
+
+            const formContainer = document.querySelector(".container");
+            formContainer.style.display = "none";
+
+            const congratulationmessage = congratulationBox.querySelector(".part1 p");
+            congratulationmessage.textContent=`You selected ${selectedRating} out of 5`
+        
             ratingButtons.forEach(btn => {
                 btn.classList.remove("selected");
             });
         } else {
             console.log("Por favor, selecciona una calificación antes de enviar.");
         }
+        
     });
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    formulario();
+});
